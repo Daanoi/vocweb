b6b07d469e1acf0f45569b60b4d81f7110add189
Daanoi <dan.kim.daniel.06@gmail.com>
2024-08-18 13:36:21 +0900

    Update vocab.py

--- Web1/vocab.py
+++ Web1/vocab.py
@@ -10,3 +10,29 @@ with st.container():
     st.write("Links:")
     st.write("  [DSAT Vocab Game 2024 >](http://localhost:8000)")
     st.write("  [DSAT Vocab Game 2024 Word List >](https://docs.google.com/document/d/1IOYDR5jCS1qen4OxwLI4_s-urNRJS3diMBx7qHMSRRM/edit)")
+
+
+
+#CONTACT
+
+with st.container():
+    st.write("----")
+    st.header("Send any vocabs you wish to be added!")
+    st.header("ex) 'Dan' (vocab) = 'genius' (synonym)")
+    st.write('##')
+
+    contact_form = """
+    <form action="https://formsubmit.co/dan.kim.daniel.06@gmail.com" method="POST">
+        <input tupe="hidden" name=_captcha" value="false">
+        <input type="text" name="name" placeholder="Your Name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Enter vocab with its synonym here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
