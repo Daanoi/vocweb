a3cc2931d4e4a2e5f84f888be2fba0df4ca97724
Daanoi <dan.kim.daniel.06@gmail.com>
2024-08-18 13:51:18 +0900

    Add files via upload

---
+++ Web1/Web1/vocab.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="DSAT Vocab", page_icon=":nerd_face:", layout="wide")
+
+#HEADER
+with st.container():
+    st.subheader("Welcome :wave:")
+    st.title("DSAT Vocab Game")
+    st.write("Links:")
+    st.write("  [DSAT Vocab Game 2024 >](http://localhost:8000)")
+    st.write("  [DSAT Vocab Game 2024 Word List >](https://docs.google.com/document/d/1IOYDR5jCS1qen4OxwLI4_s-urNRJS3diMBx7qHMSRRM/edit)")
